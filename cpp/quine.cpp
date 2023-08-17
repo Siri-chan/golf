37155d9626eab438e09c023b55b7c1fd207c13b2
Kira <54922675+Siri-chan@users.noreply.github.com>
2023-08-17 10:55:28 +1000

    Create quine.cpp

---
+++ cpp/quine.cpp
@@ -0,0 +1,9 @@
+/*** Pascal's Triangle in C++
+**** https://code.golf/pascals-triangle#cpp
+* Copyright (c) Kira K. 2023
+** Uses R" strings */
+//BEGIN
+#include<cstdio>
+int main(){char s[]=R"(#include<cstdio>
+int main(){char s[]=R"(%s%c";printf(s,s,')');})";printf(s,s,')');}
+//END
